a8b2aa764be809e437aad39fb01340fb9f217d0f
Pepe <125204578+Indiana630@users.noreply.github.com>
2024-11-21 09:16:31 +0100

    Actualizar FrontEnd con los datos de la Base de Datos

--- src/main/frontend/app-ProyectoArt/src/app/components/artistas-page/artistas-page.component.ts
+++ src/main/frontend/app-ProyectoArt/src/app/components/artistas-page/artistas-page.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {NgForOf, SlicePipe} from "@angular/common";
+import {Artista, DatabaseServiceService, Imagen} from '../../services/database-service.service';
 
 @Component({
   selector: 'app-artistas-page',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+    imports: [RouterLink, RouterOutlet, NgForOf, SlicePipe],
   templateUrl: './artistas-page.component.html',
   styleUrl: './artistas-page.component.scss'
 })
-export class ArtistasPageComponent {}
+export class ArtistasPageComponent implements OnInit {
+
+  artistas: Artista[] = [];
+  imagenes: Imagen[] = [];
+  aviso:string = "";
+
+  //Inicializamos los services
+  constructor(private databaseService: DatabaseServiceService) {}
+
+  //Cargamos nuestros artistas y su base de datos de Imagenes
+  ngOnInit(): void{
+
+    this.databaseService.getAllArtistas().subscribe((data: Artista[]) =>{
+      this.artistas = data;
+    })
+    this.databaseService.getAllImagenes().subscribe((data: Imagen[]) =>{
+      this.imagenes = data;
+    })
+  }
+
+
+  //Funcion para encontrar la Imagen destacada del Artista
+  findFirstImageOfArtist(artista: Artista){
+    let imgSrc = "";
+    this.imagenes.forEach(imagen => {
+      if (imagen.artista.id === artista.id){
+        imgSrc = imagen.url;
+      }
+    })
+
+    return imgSrc;
+  }
+
+
+
+
+
+
+
+
+
+
+  /////AVISO ERROR/////
+  setAviso(texto:string){
+    this.aviso=texto;
+    setTimeout(()=> this.aviso="",2000);
+  }
+}
